247d961398bf7aa27d99f3ecddc0718bab477197
Abhishek Shankar Choudhary <28715657+proabhishek@users.noreply.github.com>
2024-05-19 01:34:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function removeSelectedColor() {
+    var selectElement = document.getElementById("colorSelect");
+    var selectedIndex = selectElement.selectedIndex;
+    
+    if (selectedIndex !== -1) { // Check if an option is selected
+        selectElement.remove(selectedIndex);
+    }
+}
+
+const input = document.querySelector("input");
+input.addEventListener('click', removeSelectedColor);
